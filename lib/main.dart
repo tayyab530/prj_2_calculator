efa2f1169ec51f38582001b8b36b3443f297d78c
Tayyab Ahmed <mmtayyab530@gmail.com>
2024-06-08 17:29:10 +0500

    - Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import './screens/home_screen.dart';
@@ -12,7 +13,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: FlutterSplashScreen.gif(
+        gifPath: 'assets/calculator.gif',
+        gifWidth: 269,
+        gifHeight: 474,
+        nextScreen: const HomeScreen(),
+        duration: const Duration(seconds: 5),
+        onInit: () async {
+          debugPrint("onInit");
+        },
+        onEnd: () async {
+          debugPrint("onEnd 1");
+        },
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
